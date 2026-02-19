ea36b65f87bb7de67aaa7198187d8b409f87f868
Adrian Wu <adrianwu40@gmail.com>
2026-02-19 16:42:59 -0600

    Update site title to Giraffe and Potato

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ cat > index.html << 'HTMLHEAD'
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Photo Collage</title>
+  <title>Giraffe and Potato</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
